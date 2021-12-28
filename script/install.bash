f0df01d75014b9d943777bb9d7ef4b3f7a0b907f
ecervera <Enric Cervera>
2021-12-28 08:52:10 +0100

    Configuration for Google Colab

--- script/install.bash
+++ script/install.bash
@@ -3,6 +3,8 @@
 sudo apt update
 sudo apt install -y libbluetooth-dev
 
-pip install jupyter matplotlib pybluez
+python3 -m pip install jupyter matplotlib pybluez jupyter_http_over_ws
 python3 -m pip install --pre nxt-python
 python3 -m pip install ev3_dc
+jupyter serverextension enable --py jupyter_http_over_ws
+
